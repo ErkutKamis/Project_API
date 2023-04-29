10bc1b0d5fbe98ff29219cc40fcb80500ed8fe9f
Erkut Kamış <erkutkamis@gmail.com>
2023-04-29 12:05:54 +0300

    Project_API
    Asp.Net MVC ile API geliştirrme

---
+++ Project_API/Controllers/StudentController.cs
@@ -0,0 +1,67 @@
+﻿using Project_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Project_API.Controllers
+{
+    public class StudentController : ApiController
+    {
+        StudentDBEntities db = new StudentDBEntities();
+
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetStudent()
+        {
+            List<Student> list = db.Students.ToList();
+            return Ok(list);
+        }
+
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetStudentById(int id)
+        {
+            var std = db.Students.Where(model => model.StudentID == id).FirstOrDefault();
+            return Ok(std);
+        }
+
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult StudentInsert(Student s)
+        {
+            db.Students.Add(s);
+            db.SaveChanges();
+            return Ok();
+        }
+
+        [System.Web.Http.HttpPut]
+        public IHttpActionResult StudentUpdate(Student s)
+        {
+            var std = db.Students.Where(model => model.StudentID == s.StudentID).FirstOrDefault();
+            if (std != null)
+            {
+                std.StudentID = s.StudentID;
+                std.Name = s.Name;
+                std.SurName = s.SurName;
+                std.Note = s.Note;
+                db.SaveChanges();
+            }
+            else
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        [System.Web.Http.HttpDelete]
+        public IHttpActionResult StudentDelete(int id)
+        {
+            var std = db.Students.Where(model => model.StudentID == id).FirstOrDefault();
+            db.Entry(std).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
---
+++ Project_API/Controllers/StudentMVCController.cs
@@ -0,0 +1,122 @@
+﻿using Project_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Project_API.Controllers
+{
+    public class StudentMVCController : Controller
+    {
+        // GET: StudentMVC
+        HttpClient client = new HttpClient();
+
+        public ActionResult Index()
+        {
+            List<Student> student_list = new List<Student>();
+            client.BaseAddress = new Uri("https://localhost:44393/api/Student");
+            var response = client.GetAsync("Student");
+            response.Wait();
+
+            var test = response.Result;
+            if(test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<Student>>();
+                display.Wait();
+                student_list = display.Result;
+            }
+
+            return View(student_list);
+        }
+
+        public ActionResult Create() 
+        {
+            return View();
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public ActionResult Create(Student std)
+        {
+            client.BaseAddress = new Uri("https://localhost:44393/api/Student");
+            var response = client.PostAsJsonAsync<Student>("Student", std);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View("Create");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Student s = null;
+            client.BaseAddress = new Uri("https://localhost:44393/api/Student");
+            var response = client.GetAsync("Student?id=" + id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Student>();
+                display.Wait();
+                s = display.Result;
+            }
+            return View(s);
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public ActionResult Edit(Student s)
+        {
+            client.BaseAddress = new Uri("https://localhost:44393/api/Student");
+            var response = client.PutAsJsonAsync<Student>("Student", s);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View("Edit");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Student s = null;
+            client.BaseAddress = new Uri("https://localhost:44393/api/Student");
+            var response = client.GetAsync("Student?id=" + id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Student>();
+                display.Wait();
+                s = display.Result;
+            }
+            return View(s);
+        }
+        [System.Web.Mvc.HttpPost , System.Web.Mvc.ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            client.BaseAddress = new Uri("https://localhost:44393/api/Student");
+            var response = client.DeleteAsync("Student/"+id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View("Delete");
+            
+        }
+    }
+}
